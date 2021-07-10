959ffbbe5fca49a1cdcb393cb362c43bbec020e3
Invis <46322535+InvisRaidinq@users.noreply.github.com>
2021-07-10 16:01:51 +0100

    Easy registering of commands and listeners

--- Bungee/src/main/java/xyz/invisraidinq/rocketapi/bungee/RocketBungeePlugin.java
+++ Bungee/src/main/java/xyz/invisraidinq/rocketapi/bungee/RocketBungeePlugin.java
@@ -1,6 +1,11 @@
 package xyz.invisraidinq.rocketapi.bungee;
 
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.List;
 
 public abstract class RocketBungeePlugin extends Plugin {
 
@@ -16,4 +21,14 @@ public abstract class RocketBungeePlugin extends Plugin {
 
     public abstract void onServerStart();
     public abstract void onServerStop();
+
+    public void registerListeners(List<Listener> listeners) {
+        listeners.forEach(listener ->
+                ProxyServer.getInstance().getPluginManager().registerListener(this, listener));
+    }
+
+    public void registerCommands(List<Command> commands) {
+        commands.forEach(command ->
+                ProxyServer.getInstance().getPluginManager().registerCommand(this, command));
+    }
 }
